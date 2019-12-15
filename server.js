4836211b8acfde47a1abbb370b00021fd5211b31
Matthew Murray <matthewtmurray@gmail.com>
2019-12-15 22:49:28 +0000

    updates

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
-const initializePassport = require('./passport-config')
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport, 
+    (email) => users.find(user.email === email)
+);
 
 const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({extended: false}));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req,res)=>{
     res.render("index.ejs", {name: "Matthew"});
